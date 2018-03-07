1b022bdfd42af902d2a108f47342aa0be767462a
ZekeKoch <zeke@akrasia.org>
2018-03-07 13:08:30 -0800

    mostly moved code to Tickets, reduced globals, var to let

--- pm trello.js
+++ pm trello.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var google = require('googleapis');
-var Trello = require("trello") // https://github.com/norberteder/trello
+var Trello = require('trello') // https://github.com/norberteder/trello
 var secrets = require('./secrets.json');
 
 // this holds the tickets coming out of our google spreadsheet
@@ -17,7 +17,9 @@ class Ticket
     this.pm = pm;
     this.pgm = pgm;
     this.scrumTeam = scrumTeam;
-    this.quadmester = quadmester;
+    if (quadmester)
+      this.quadmester = quadmester.toLowerCase();
+
     this.priority = priority;
   }
 }
@@ -27,80 +29,100 @@ class Tickets
   constructor() 
   {
     this.items = [];
+    this.pms = {};
+    this.quadmesters = {};
+    this.scrumTeams = {};
+    this.scrumTeamColors = 
+    {
+      Integrations:"pink",
+      Contributor:"red",
+
+      MIB:"blue",
+      Organizations:"sky",
+      Partner:"purple",
+
+      eComm:"green",
+      SNAP:"lime",
+
+      Infra:"yellow",
+      SRE:"orange",
+      TBD:"black"
+    };
   }
 
   add(ticket) 
   {
-    if (this.add.count == undefined)
-      this.add.count = 0;
+    // poor man's static variable, there must be a better idiom in js
+    if (this.length == undefined)
+      this.length = 0;
     else
-      this.add.count++;
+      this.length++;
 
-    // associative array for the pms
+    // add the ticket to the list of tickets
+    this.items[this.length] = ticket;
+
+    // create an associative array for the pms & scrumteams
     this.pms[ticket.pm] = ticket.pm;
+    this.scrumTeams[ticket.scrumTeam] = ticket.scrumTeam;
 
-    this.items[this.add.count] = ticket;
+    // if it's a new team their color is null
+    if(this.scrumTeamColors[ticket.scrumTeam] == null) // null or undefined
+      this.scrumTeamColors[ticket.scrumTeam] = 'null';
+
+    // skip the garbage quadmesters
+    let quadmester = ticket.quadmester;
+    switch (quadmester) {
+      case 'duplicate':
+      case undefined:
+      case 'later':
+      case 'target period':
+        // console.log('skipping ' + val);
+        // do nothing          
+        break;
+      default:
+        // console.log('found ' + val);
+        // create the quadmesters
+        this.quadmesters[quadmester] = quadmester;
+        
+        // add 7 sprints per quadmester
+        for(let sprint = 1;sprint <= 7;sprint++)
+        {
+          this.quadmesters[quadmester + "-" + sprint] = quadmester + " S" + sprint;
+        }          
+        break;
+    }
+
     return ticket;
   }
 
   getQuad(quad)
   {
     // this is just the tickets that are in the current quadmester
-    return currentQuadsTickets = this.items.filter(
+    return this.items.filter(
       function(ticket) 
       {
         return ticket.quadmester == quad;
       }
-    );
-    return
+    );  
   }
-
 }
 
-var tickets = new Tickets()
+var tickets = new Tickets();
 
 // setup trello as me
 var trello = new Trello(secrets.trelloKey, secrets.trelloToken);
 
 // the pm board
-var boardId = secrets.boardId;
-var sprintBoard = secrets.sprintBoard;
-
-var sheetId = secrets.sheetId;
-var keyId = secrets.keyId;
-var auth = "API_KEY";
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/sheets.googleapis.com-nodejs-quickstart.json
+// zeke: I don't know if this line does anything...
 var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];  
-
-function handleArchiveCard(error) 
-{
-    if (error) console.log("couldn't delete card")
-    else
-    { 
-        console.log("delete card");
-    }
-}
-
-function handleAddCard(error, trelloCard)
-{
-  if (error) {
-      console.log('Could not add card:', error);
-  }
-  else {
-      console.log('Added card:', trelloCard);
-  }
-}
-
-var rows;
-var pms = {};
-var themes = {};
-var quadmesters = {};
 
 // get sheet from google, this also kicks off the rest of the code
 // yay asyncness
 getSheet();
+
 
 function archiveAllLists()
 {
@@ -108,87 +130,72 @@ function archiveAllLists()
 }
 
 function getSheet() {
-  var sheets = google.sheets('v4');
+  let rows = {};
+  let sheets = google.sheets('v4');
   sheets.spreadsheets.values.get({
-    auth: auth,
+    auth: "API_KEY",
     key: secrets.keyId,
     spreadsheetId: secrets.sheetId,
-    range: 'Consolidated PL!A2:P500', // somehow I'm still getting the header row so I'm not sure the range is doing anything
-  }, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
+    range: 'Consolidated PL!A2:P500',
+  }, 
+  function(err, response) 
+  {
+    if (err) 
+    {
+      console.log('The google sheets API returned an error: ' + err);
       return;
     }
-    var rows = response.data.values;
-    if (rows.length == 0) {
+    let rows = response.data.values;
+    if (rows.length == 0) 
+    {
       console.log('No data found.');
-    } else {
+    } 
+    else 
+    {
       // skip the header row
-      for (var i = 0; i < rows.length; i++) {
-        var row = rows[i];
+      for (let i = 0; i < rows.length; i++) 
+      {
+        let row = rows[i];
 
         // icky constants, i know...
-        var ticket = tickets.add(new Ticket(row[0], row[1], row[2], row[3], row[5], row[7], row[8], row[9], row[11], row[15]));
-
-        val = new String(tickets[i].quadmester).toLowerCase();
-        switch (val) {
-          case 'duplicate':
-          case 'undefined':
-          case 'later':
-          case 'target period':
-            // console.log('skipping ' + val);
-            // do nothing          
-            break;
-          default:
-            // console.log('found ' + val);
-            quadmesters[val] = val;
-            break;
-        }
+        let ticket = tickets.add(new Ticket(row[0], row[1], row[2], row[3], row[5], row[7], row[8], row[9], row[11], row[15]));
       }
-      // print out the quadmesters, aren't they pretty?
-      for (const quad in quadmesters) {
-        if (quadmesters.hasOwnProperty(quad)) {
-          const q = quadmesters[quad];
-          for(var i = 1;i <= 7;i++)
-          {
-            quadmesters[q + "-" + i] = q + " S" + i;
-          }          
-        }
-      }
-      console.log(quadmesters);
 
       // i now have my tickets so I need to create the trello board
       // i'm assuming that the previous stuff works somewhat synchronously, but I'm bluffing
-      // never a great thing when you're programming, but it seems to be working now...
+      // never a great thing when you're programming, but it seems to be working for now...
 
       // i'm new to promises, so i'm sure this syntax is ugly
       // why oh why is js so weird...
 
       // first I want to create a list per quadmester
-      trello.getListsOnBoard(boardId)
-      .then((lists) => // once the function return it calls then()
+      trello.getListsOnBoard(secrets.boardId)
+      .then((trelloLists) => // once the function return it calls then()
       {
-        // loop over the quadmesters and make sure I have a 
-        // list for each one
-        for (var quad in quadmesters)
+        // a trelloList has id, idBoard, name, pos etc
+
+        // loop over the quadmesters and get a list of tickets for each one
+        for (let quad in tickets.quadmesters)
         {
-          console.log("processing " + quadmesters[quad]);
-          var hasList = false;
+          console.log("processing " + tickets.quadmesters[quad]);
 
           // look for any items in the current quadmester
-          // lists sn an object array 
-          var currentItem = lists.find(
+
+          // look for a trelloList for the current quadmester  
+          let list = trelloLists.find(
             function(element) 
             {
               // i lowercased the quadmesters since the pdgms are sloppy
-              return element.name.toLowerCase() == quadmesters[quad];
+              return element.name.toLowerCase() == tickets.quadmesters[quad];
             }
           );
-          if (currentItem)  // this means I found the quadmester
+
+          // if I find that trelloList then I don't need to create it 
+          if (list)
           {
-              // if I already have a list then I can start adding items to that
-              console.log('found list:' + quadmesters[quad]);
-              addCardsToListFromQuadmester(currentItem.id, tickets.getQuad(quadmesters[quad]));
+              // great, I can start adding items to that
+              console.log('found list:' + tickets.quadmesters[quad]);
+              addCardsToListFromQuadmester(list.id, tickets.getQuad(tickets.quadmesters[quad]));
           }
           else
           {
@@ -196,11 +203,11 @@ function getSheet() {
             //          fix this when I get back from lunch (or later)
 
             // otherwise i need to create a new list
-            trello.addListToBoard(boardId, quadmesters[quad])
-            .then((currentItem) => // this can take a while so I use a promise (to get called when I'm done)
+            trello.addListToBoard(secrets.boardId, tickets.quadmesters[quad])
+            .then((list) => // this can take a while so I use a promise (to get called when I'm done)
             {
-              console.log('created list: ' + currentItem.name);  
-              addCardsToListFromQuadmester(currentItem.id, currentQuadsTickets);
+              console.log('created list: ' + list.name);  
+              addCardsToListFromQuadmester(list.id, currentQuadsTickets);
             })
             .catch((error) =>
             {
@@ -208,9 +215,9 @@ function getSheet() {
             });
           }
         }
-        return lists;
+        return trelloLists;
       })
-      .then((lists) => {
+      .then((trelloLists) => {
         // i've created all of the lists now I should order them
         for (const key in object) {
           if (object.hasOwnProperty(key)) {
@@ -218,7 +225,7 @@ function getSheet() {
             
           }
         }
-        console.log(lists);          
+        console.log(trelloLists);          
       })
       .catch((error) => {
         console.log('error in get list on board' + error);
@@ -229,17 +236,23 @@ function getSheet() {
 
 function addCardsToListFromQuadmester(listId, tickets)
 {
-  for (const ticket in tickets) {
-    if (tickets.hasOwnProperty(ticket)) 
+  if (tickets)
+  {
+    for (const ticket in tickets) 
     {
-      addCardsToListFromTicket(listId, tickets[ticket]);                  
+        addCardsToListFromTicket(listId, tickets[ticket]);                  
     }
+  }
+  else
+  {
+    console.log('tickets is undefined');
   }
 }
 
 function addCardsToListFromTicket(listId, ticket)
 {
-  var title = ticket.feature + " (" + ticket.swag + ")";
+  let title = ticket.feature + " (" + ticket.swag + ")";
+  console.log('adding ticket to ' + listId + ':' + title);
   trello.addCard(title, ticket.description, listId)
   .then((data) => 
   {
